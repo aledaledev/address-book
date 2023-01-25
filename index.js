471ffbdd4706bb8910e8a7e565bcf7d34f1dcdba
aledale <ale.salas271102@gmail.com>
2023-01-24 23:58:29 -0300

    adding form to pug page

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import {connect, addContact, getContacts} from './src/model/database.js'
 
+//se muestra de lado del equipo/terminal (servidor)
 const app = express()
 
 app.set('views','./views')
@@ -9,10 +11,22 @@ app.use(express.static('./views'))
 app.use(express.static('./src'))
 app.use(express.static('./css'))
 
-app.get('/', (req,res) => {
-    res.render('index', {title: "pug vs pilu", text:"texto desde backend"})
+app.get('/', async (req,res) => {
+    res.render('index', {title: "puggy", contacts: await getContacts()})
+})
+
+app.get('/add/:name/:phone', (req,res) => {
+    const {name, phone} = req.params
+    addContact({name,phone})    
+    res.redirect('/')
+})
+
+app.use((req,res) => {
+    res.status(404).json({message:"Page not found!"})
 })
 
 app.listen(3000, () => {
+    //esta
+    connect()
     console.log(`Server running on port 3000`);
 })
--- src/model/database.js
+++ src/model/database.js
@@ -1,5 +1,5 @@
 import mysql from 'mysql2'
-import config from '../config'
+import config from '../config.js'
 
 const connection = mysql.createConnection({
     host: config.host,
@@ -8,6 +8,21 @@ const connection = mysql.createConnection({
     password: config.password,
 })
 
+export const addContact = ({name, phone:phone_number}) => {
+    connection.query(`INSERT INTO contacts SET ?`, {name, phone_number:Number(phone_number)}, (err,result) => {
+        if(err) throw err
+        console.log(result);
+    })   
+}
+
+export const getContacts = async () => {
+    await connection.query(`SELECT * FROM contacts`, (err,result) => {
+        if(err) throw err
+        return result
+    })  
+}
+
+//parece que siempre se mantiene conectado
 export const connect = () => {
     connection.connect(err => {
         if(err) throw err
